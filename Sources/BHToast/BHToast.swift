3807958fd9577accd971e8eaac391b2e7c944d37
cody <qudgus1984@naver.com>
2022-12-27 20:48:19 +0900

    [Feat] Toast Message Foundation

--- Sources/BHToast/BHToast.swift
+++ Sources/BHToast/BHToast.swift
@@ -1,13 +1,33 @@
 import UIKit
 
-public struct BHToast {
-    public private(set) var text = "Hello, World!"
+//public struct BHToast {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//
+//    func showBHToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0), view: UIView) {
+//        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 75, y: view.frame.size.height-100, width: 150, height: 35))
+//        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+//        toastLabel.textColor = UIColor.white
+//        toastLabel.font = font
+//        toastLabel.textAlignment = .center;
+//        toastLabel.text = message
+//        toastLabel.alpha = 1.0
+//        toastLabel.layer.cornerRadius = 10;
+//        toastLabel.clipsToBounds  =  true
+//        view.addSubview(toastLabel)
+//        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
+//             toastLabel.alpha = 0.0
+//        }, completion: {(isCompleted) in
+//            toastLabel.removeFromSuperview()
+//        })
+//    }
+//}
 
-    public init() {
-    }
-    
-    func showBHToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0), view: UIView) {
-        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 75, y: view.frame.size.height-100, width: 150, height: 35))
+public class BHToast: UIViewController {
+    func showBHToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         toastLabel.textColor = UIColor.white
         toastLabel.font = font
@@ -16,9 +36,9 @@ public struct BHToast {
         toastLabel.alpha = 1.0
         toastLabel.layer.cornerRadius = 10;
         toastLabel.clipsToBounds  =  true
-        view.addSubview(toastLabel)
+        self.view.addSubview(toastLabel)
         UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
-             toastLabel.alpha = 0.0
+            toastLabel.alpha = 0.0
         }, completion: {(isCompleted) in
             toastLabel.removeFromSuperview()
         })
