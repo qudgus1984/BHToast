7210f76e53f3d018a525905e432944514d016c5d
cody <qudgus1984@naver.com>
2023-01-03 16:27:54 +0900

    [Fix] 오류 발견 후 Test Commit

--- Tests/BHToastTests/BHToastTests.swift
+++ Tests/BHToastTests/BHToastTests.swift
@@ -1,11 +1,28 @@
 import XCTest
 @testable import BHToast
 
-final class BHToastTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BHToast().text, "Hello, World!")
+import UIKit
+import BHToast
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func actionAlert(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            BHToast(title: "와 이게 되네", confirm: "확인버튼도 커스텀할 수 있네") {
+                print("completion handler 까지!?!")
+            }.show(in: self.view)
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
