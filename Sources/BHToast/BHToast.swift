0473c40c08d2f1cb65cef91ace4304621faae69f
cody <qudgus1984@naver.com>
2023-01-03 15:51:12 +0900

    [Feat] Toast Library Update

--- Sources/BHToast/BHToast.swift
+++ Sources/BHToast/BHToast.swift
@@ -1,46 +1,62 @@
 import UIKit
 
-//public struct BHToast {
-//    public private(set) var text = "Hello, World!"
-//
-//    public init() {
-//    }
-//
-//    func showBHToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0), view: UIView) {
-//        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 75, y: view.frame.size.height-100, width: 150, height: 35))
-//        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-//        toastLabel.textColor = UIColor.white
-//        toastLabel.font = font
-//        toastLabel.textAlignment = .center;
-//        toastLabel.text = message
-//        toastLabel.alpha = 1.0
-//        toastLabel.layer.cornerRadius = 10;
-//        toastLabel.clipsToBounds  =  true
-//        view.addSubview(toastLabel)
-//        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
-//             toastLabel.alpha = 0.0
-//        }, completion: {(isCompleted) in
-//            toastLabel.removeFromSuperview()
-//        })
-//    }
-//}
+public enum BHToastStyle {
+    case basic
+    case long
+    case short
+}
 
-public class BHToast: UIViewController {
-    public func showBHToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
-        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        toastLabel.textColor = UIColor.white
-        toastLabel.font = font
-        toastLabel.textAlignment = .center;
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
-        self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
+open class BHToast: UIView {
+    private let toastView = UIView()
+    private var message = UILabel()
+    
+    public convenience init(message: UILabel = UILabel()) {
+        self.init(frame: CGRect.zero)
+        self.message = message
+    }
+}
+
+extension BHToast {
+    public func showBHToast(_ rootView: UIView, text: String, font: UIFont, Style: BHToastStyle) {
+        setAttribute(text: text, font: font)
+        configureLayout(rootView)
+        setConstraints(rootView, Style: Style)
+    }
+    
+    private func configureLayout(_ rootView: UIView) {
+        rootView.addSubview(self)
+        self.addSubview(toastView)
+        toastView.addSubview(message)
+    }
+    
+    private func setConstraints(_ rootView: UIView, Style: BHToastStyle) {
+        switch Style {
+        case .basic:
+            self.message = UILabel(frame: CGRect(x: rootView.frame.size.width/2 - 75, y: rootView.frame.size.height-100, width: 150, height: 35))
+            
+        case .long:
+            self.message = UILabel(frame: CGRect(x: rootView.frame.size.width/2 - 75, y: rootView.frame.size.height-100, width: 200, height: 35))
+
+        case .short:
+            self.message = UILabel(frame: CGRect(x: rootView.frame.size.width/2 - 75, y: rootView.frame.size.height-100, width: 100, height: 35))
+
+        }
+    }
+    
+    public func setAttribute(text: String, font: UIFont) {
+        message.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        message.textColor = UIColor.white
+        message.font = font
+        message.textAlignment = .center;
+        message.text = text
+        message.alpha = 1.0
+        message.layer.cornerRadius = 10;
+        message.clipsToBounds  =  true
+        toastView.addSubview(message)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: { [self] in
+            message.alpha = 0.0
+        }, completion: { [self](isCompleted) in
+            message.removeFromSuperview()
         })
     }
 }
